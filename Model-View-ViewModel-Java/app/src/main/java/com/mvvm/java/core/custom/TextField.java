c768a9e98fdd15bd7ea9a7688632a2ad154e52c7
GiangNB2 <giangnb2@rikkeisoft.com>
2022-09-30 10:33:29 +0700

    create text field custom in mvvm java

--- Model-View-ViewModel-Java/app/src/main/java/com/mvvm/java/core/custom/TextField.java
+++ Model-View-ViewModel-Java/app/src/main/java/com/mvvm/java/core/custom/TextField.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 
 import com.mvvm.java.R;
 import com.mvvm.java.databinding.TextFieldBinding;
@@ -36,6 +37,30 @@ public class TextField extends LinearLayout {
 
         if (attrs != null) {
             TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.TextField, 0, 0);
+
+            String hintTextField = typedArray.getString(R.styleable.TextField_hintTextField);
+            int hintTextFieldRes = typedArray.getInt(R.styleable.TextField_hintTextField, 0);
+            if (hintTextField != null) {
+                this.binding.textInputLayout.setHint(hintTextField);
+            } else if (hintTextFieldRes != 0) {
+                this.binding.textInputLayout.setHint(hintTextFieldRes);
+            }
+
+            String inputTextField = typedArray.getString(R.styleable.TextField_inputTextField);
+            int inputTextFieldRes = typedArray.getInt(R.styleable.TextField_inputTextField, 0);
+            if (inputTextField != null) {
+                this.binding.textInputEditText.setText(inputTextField);
+            } else if (inputTextFieldRes != 0) {
+                this.binding.textInputEditText.setText(inputTextFieldRes);
+            }
         }
+    }
+
+    public void setHintText(String hintText) {
+        this.binding.textInputLayout.setHint(hintText);
+    }
+
+    public void setHintText(@StringRes int hintRes) {
+        this.binding.textInputLayout.setHint(hintRes);
     }
 }
